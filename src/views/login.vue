d1a1c2df0ca0d5242d588cf8e176f018682447c9
Lin Zheng <inmount@live.cn>
2023-04-11 10:57:12 +0800

    新增：从Suyaa.Apis仓库迁入

---
+++ src/views/login.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import LoginForm from '@/components/LoginForm.vue'
+import bgImg from '/login/bg.jpg'
+</script>
+
+<template>
+    <div class="bg"><img :src="bgImg"></div>
+    <LoginForm />
+</template>
+
+<style scoped>
+.bg {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+}
+
+.bg img {
+    display: block;
+    width: 100%;
+    min-width: 1920px;
+}
+</style>
